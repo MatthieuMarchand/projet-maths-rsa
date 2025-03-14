7a00437d9fad416d2e33819d0399644e2763fa4a
Arthaud <arthaudp33@gmail.com>
2025-03-14 15:49:56 +0100

    ⚡️ use faster power operation

--- core/encryption.py
+++ core/encryption.py
@@ -4,7 +4,7 @@ dic = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz!?.' "
 def encrypt(message, public_key):
     N, c = public_key
     char_map = {char: idx + 1 for idx, char in enumerate(dic)}
-    encrypted_message = [(char_map[char] ** c) % N for char in message]
+    encrypted_message = [(pow(char_map[char], c)) % N for char in message]
     return encrypted_message
 
 
@@ -12,5 +12,5 @@ def decrypt(encrypted_message, private_key, public_key):
     N, c = public_key
     
     reverse_map = {index + 1: char for index, char in enumerate(dic)}
-    message = ''.join(reverse_map[(num ** private_key) % N] for num in encrypted_message)
+    message = ''.join(reverse_map[(pow(num, private_key)) % N] for num in encrypted_message)
     return message
