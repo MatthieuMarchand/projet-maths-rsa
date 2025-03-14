04f54bf61cb14381a111546fdb250182b54c09d5
Arthaud <arthaudp33@gmail.com>
2025-03-14 17:11:43 +0100

    :sparkles: Store encrypted msg in file

--- main.py
+++ main.py
@@ -3,10 +3,18 @@ from core.key import generate_keys
 
 public_key, private_key = generate_keys()
 
-message = "bonjour. attention la derniere version du samsung pro est desormais en vente libre sans le protocole new generation. il se peut qu'une version plus moderne soit mise sur le marche mais pour l'instant nous le laissons tel quel pour voir si les consommateurs l'achetent et si oui a quelle quantite. nous lancerons un plan de restructuration du reseau courant janvier ce qui nous permettra de tester ce nouveau produit de facon plus precise. a noter aussi que le nokia a perdu enormement de points sur le marche japonnais. il faudrait peut etre parler aussi de ce point lors de la prochaine reunion. bien a vous."
+print("Public key:", public_key)
+print("Private key:", private_key)
+
+message = "ce message est encrypte avec une methode revolutionnaire"
 print("Encryption")
 encrypted = encrypt(message, public_key)
 print("Message encrypté:", encrypted)
+
+f = open("encrypted.txt", "w")
+f.write("\n".join([str(i) for i in encrypted]))
+f.close()
+
 print("Décryptage")
 decrypted = decrypt(encrypted, private_key, public_key)
 print("Message décrypté:", decrypted)
