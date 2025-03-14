d89ea905f5b57b9d80d412f8089b83d292e9a951
Arthaud <arthaudp33@gmail.com>
2025-03-14 17:11:58 +0100

    :sparkles: Use random large prime numbers

--- core/key.py
+++ core/key.py
@@ -1,9 +1,23 @@
-from sympy import mod_inverse
+import random
+
+from sympy import isprime, mod_inverse
+
+
+def generate_prime(min_val=10_000, max_val=99_999):
+    while True:
+        num = random.randint(min_val, max_val)
+        if isprime(num):
+            return num
 
 
 # This is a fake random function for demo purposes
 def generate_prime_pair():
-    return [101, 103]
+    p = generate_prime()
+    q = generate_prime()
+    while p == q:
+        p = generate_prime()
+
+    return [p, q]
 
 
 def generate_keys():
@@ -12,7 +26,7 @@ def generate_keys():
     N = p * q
     n = (p - 1) * (q - 1)
 
-    c = 65537
+    c = pow(2, 16) + 1
 
     d = mod_inverse(c, n)
 
